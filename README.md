bd86fb3c7ef60c31f4a2eb502ebccf0412a609cd
svenboertjens <boertjens.sven@gmail.com>
2024-06-09 21:20:52 +0200

    Debugged

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # empty lines
 
-Hey there. This repo contains the source of my 'empty lines' VSCode extension.
+Hey there. This is my `empty lines` extension to handle empty lines in your code.
 
-It's a relatively simple extension. It does:
-1. Ensure there's a maximum of `x` empty lines in between code,
-2. Ensure there's `y` empty lines at the end of your code, and
-3. Fix indentation on empty lines.
+It's a relatively simple extension. It does two things:
+1. Ensure there's a maximum of `x` empty lines in between code, and
+2. Ensure there's `y` empty lines at the end of your code.
 
 Documentation about the source code can be found in comments across the code.
--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as os from 'os';
 
 // Function to return whether a line is empty, excluding whitespace characters
 function isLineEmpty(line: string): boolean {
-	return line.trim().length === 0;
+    return line.trim().length === 0;
 }
 
 // List of languages that use curly brackets to indicate code blocks
@@ -20,7 +20,6 @@ const bracketPairLanguages: string[] = [
     "typescript",
     "json",
     "rust",
-    "go",
     "scala",
     "php",
     "ruby",
@@ -33,43 +32,17 @@ const bracketPairLanguages: string[] = [
     "shellscript"
 ];
 
+// List of languages to always exclude of indentation reading
+const excludeIndentationLanguages: string[] = [
+    "python"
+];
+
 // Function to get the indentation level of a line
 function getIndentation(line: string): number {
     // Match leading whitespace characters using an expression
     const match = line.match(/^\s*/);
     // If there's a match, return the length of the matched whitespace. Else return 0
     return match ? match[0].length : 0;
-}
-
-// Function to ensure a number isn't negative
-function ensureNonNegative(num: number): number {
-    // If num is less than 0, return 0, otherwise return num
-    return num < 0 ? 0 : num;
-}
-
-// Function to add indentation to a line
-function addIndentation(line: string, indentationCharacter: string, requiredIndentation: number): string {
-    // Get the current indentation of the line
-    const currentIndentation: number = getIndentation(line);
-    const toAdd: number = ensureNonNegative(requiredIndentation - currentIndentation);
-
-    // Add additional indentation
-    const indentedLine: string = indentationCharacter.repeat(toAdd) + line;
-
-    return indentedLine;
-}
-
-// Function to get the indentation used in the current file
-function getIndentationType(document: vscode.TextDocument): string {
-    const firstNonEmptyLine = document.getText().match(/[^\s]/m);
-    if (firstNonEmptyLine) {
-        const indentation = firstNonEmptyLine[0].match(/^\s*/);
-        if (indentation) {
-            return indentation[0];
-        }
-    }
-    // Default to using four spaces for indentation
-    return "    ";
 }
 
 const stringCharacters: string[] = [
@@ -95,16 +68,12 @@ function verifyBrackets(line: string): number {
                 inString = true;
                 stringChar = char;
             }
-            // Check for bracket opens and closes
+        // Check for bracket opens and closes
         } else if (!inString) {
             if (char == "{") {
-                bracketOpen = 1;
+                bracketOpen++;
             } else if (char == "}") {
-                if (bracketOpen == 1) {
-                    bracketOpen = 0;
-                } else {
-                    bracketOpen = 2;
-                }
+                bracketOpen--;
             }
         }
     }
@@ -118,15 +87,14 @@ function isLineFocused(focusedLine: number, currentLine: number, isFocused: bool
 }
 
 function removeEmptyLines(
-        document: vscode.TextDocument,
-        i: number,
-        totalLines: number,
-        focusedLine: number,
-        emptyLinesBetween: number,
-        focusOn: number,
-        fixIndentation: boolean,
-        indentationCharacter: string
-    ): [string[], number] {
+    document: vscode.TextDocument,
+    i: number,
+    totalLines: number,
+    focusedLine: number,
+    emptyLinesBetween: number,
+    focusOn: number,
+    useBrackets: boolean
+): [string[], number] {
     let currLine: string = document.lineAt(i).text;
     let emptyAmount: number = 0;
     let emptyLines: string[] = [];
@@ -135,7 +103,8 @@ function removeEmptyLines(
 
     // Get the indentation of the line before
     let indentationBefore: number = 0;
-    if (fixIndentation && i > 0) {
+    let currentIndentation: number = 0;
+    if (focusOn == 2 && !useBrackets && i > 0) {
         const lineBefore: string = document.lineAt(i - 1).text;
         indentationBefore = getIndentation(lineBefore);
 
@@ -143,20 +112,25 @@ function removeEmptyLines(
         if (verifyBrackets(lineBefore) == 1) {
             indentationBefore++;
         }
+
+        currentIndentation = indentationBefore;
     }
 
     // Check whether the code block before the empty lines is selected
-    let currentIndentation = getIndentation(currLine);
     if (focusOn == 2 && i > 0) {
-        let indentation: number;
+        let indentation: number = currentIndentation;
 
         for (let j: number = i - 1; j > -1; j--) {
             // Get the current line and its indentation
             const lineBefore: string = document.lineAt(j).text;
-            indentation = getIndentation(lineBefore);
-            
+            if (!useBrackets) {
+                indentation = getIndentation(lineBefore);
+            }
+
             // Check whether we need to stop checking lines
-            if (isFocused || indentation !== currentIndentation) {
+            if (isFocused) {
+                break;
+            } else if ((useBrackets && verifyBrackets(lineBefore) !== 0) || indentation !== currentIndentation) {
                 break;
             }
 
@@ -185,35 +159,21 @@ function removeEmptyLines(
         currLine = document.lineAt(i).text;
     }
 
-    // Compare current indentation to before/after indentation and correct it if necessary
-    if (fixIndentation && i !== totalLines) {
-        let indentationAfter = getIndentation(currLine);
-
-        // Increment indentation by 1 if the next line closes a code block (thus has an indentation lower by one)
-        if (verifyBrackets(currLine) == 2) {
-            indentationAfter++;
-        }
-
-        if (indentationBefore == indentationAfter && currentIndentation != indentationBefore) {
-            currentIndentation = indentationBefore;
-        }
-    }
-
     if (i < totalLines && focusOn === 2) {
-        let indentation: number = currentIndentation;
-
-        while (indentation === currentIndentation) {
-            // Add line to the non-empty row
-            nonEmptyLines.push(currLine);
-
-            // Get the indentation of the current line
-            indentation = getIndentation(currLine);
-
+        while (i < totalLines) {
             // Increment iterator and check if end is reached
             i++;
-            if (i === totalLines || indentation !== currentIndentation) {
+            if (i === totalLines) {
+                // Set `isFocused` to true to make sure nothing happens, as the last empty lines are handles differently
+                isFocused = true;
+                break;
+                // Check whether indentation levels differ
+            } else if ((useBrackets && verifyBrackets(currLine) !== 0) || (!useBrackets && getIndentation(currLine) !== currentIndentation)) {
                 break;
             }
+
+            // Add line to the non-empty row
+            nonEmptyLines.push(currLine);
 
             // Check if the current line is focused
             isFocused = isLineFocused(focusedLine, i, isFocused);
@@ -221,6 +181,8 @@ function removeEmptyLines(
             // Get the next line
             currLine = document.lineAt(i).text;
         }
+        // Decrement `i` because one extra was added before quitting the loop
+        i--;
     }
 
     // Calculate the amount of lines to exclude
@@ -229,8 +191,8 @@ function removeEmptyLines(
     // Set `isFocused` to false if it's not to be checked for
     isFocused = focusOn !== 0 && isFocused;
 
-    // Remove all extra empty lines if the row isn't focused
-    if (!isFocused && excludeAmount > 0) {
+    // Remove all extra empty lines if the row isn't focused or isn't the last empty lines
+    if (!isFocused && excludeAmount > 0 && i !== totalLines) {
         for (let j = 0; j < excludeAmount; j++) {
             emptyLines.pop();
         }
@@ -238,8 +200,6 @@ function removeEmptyLines(
 
     // Correct all other empty lines if the current block isn't focused
     if (focusOn == 2 && emptyLinesBetween >= 0) {
-        // Add the amount of lines that'll be checked here to `i` to skip over them later
-        i += nonEmptyLines.length;
         // Check whether further empty lines should be removed
         if (!isFocused && emptyLinesBetween >= 0) {
             let trailingEmpty = 0;
@@ -270,25 +230,16 @@ function removeEmptyLines(
         }
     }
 
-    // Correct indentation of empty lines
-    if (fixIndentation && currentIndentation !== 0) {
-        // Put all lines together in the `emptyLines` list and empty the other
-        emptyLines.push(...nonEmptyLines);
-        nonEmptyLines = [];
+    // Add non-empty lines to the list
+    emptyLines.push(...nonEmptyLines);
 
-        for (let j: number = 0; j < emptyLines.length; j++) {
-            currLine = addIndentation(emptyLines[j], indentationCharacter, currentIndentation);
-            emptyLines[j] = currLine;
-        }
-    }
-
-    // Decrease iterator by one to not skip over last non-empty line, if exists
+    // Decrement i to not skip over the last checked line
     if (i !== totalLines) {
         i--;
     }
 
-    // Add non-empty lines to the list
-    emptyLines.push(...nonEmptyLines);
+    // Add the amount of lines that'll be checked here to `i` to skip over them later
+    //i += nonEmptyLines.length;
 
     return [emptyLines, i];
 }
@@ -299,18 +250,21 @@ const focusStringToNumber: { [key: string]: number } = {
     "Block": 2
 };
 
+// Variable to debounce self-applied changes
+let debounce = false;
+
 // The main function of the extension
 async function addEmptyLines() {
     const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("empty-lines");
     const isEnabled: boolean = config.get<boolean>("enabled", true);
-    // Do nothing if not enabled
-    if (!isEnabled) {
+    // Do nothing if not enabled or debouncing
+    if (!isEnabled || debounce) {
         return;
     }
-    
+
     const emptyLinesBetween: number = config.get<number>("emptyLinesBetweenCode", 1);
     const emptyLinesBehind: number = config.get<number>("emptyLinesBehindCode", 2);
-    const fixUnknownIndentation: boolean = config.get<boolean>("fixUnknownLanguageIndentation", false);
+    const alwaysUseBrackets: boolean = config.get<boolean>("alwaysRelyOnBracketMethodToRead", false);
     const preserveFocusedLines: string = config.get<string>("preserveFocusedLines", "Block");
 
     const focusOn: number = focusStringToNumber[preserveFocusedLines];
@@ -323,21 +277,19 @@ async function addEmptyLines() {
     // Get some values about the focused document
     const document: vscode.TextDocument = editor.document;
     const totalLines: number = document.lineCount;
-    const processedLines: string[] = [];
+    const processedLines: string[] = [document.lineAt(0).text];
     const focusedLine: number = editor.selection.active.line;
-    const indentationCharacter: string = getIndentationType(document);
 
     // Decide whether indentation should be fixed
     const language: string = document.languageId;
-    const fixIndentation: boolean = config.get<boolean>("fixIndentation", true) && (bracketPairLanguages.includes(language) || fixUnknownIndentation);
+    const useBrackets: boolean = (bracketPairLanguages.includes(language) || (alwaysUseBrackets && !excludeIndentationLanguages.includes(language)));
 
     // Iterate over all document lines and take action when necessary
-    for (let i = 0; i < totalLines; i++) {
+    for (let i = 1; i < totalLines; i++) {
         const line: string = document.lineAt(i).text;
-
         // Check whether the line is empty
         if (isLineEmpty(line)) {
-            const results: any[] = removeEmptyLines(document, i, totalLines, focusedLine, emptyLinesBetween, focusOn, fixIndentation, indentationCharacter);
+            const results: any[] = removeEmptyLines(document, i, totalLines, focusedLine, emptyLinesBetween, focusOn, useBrackets);
 
             // Add lines to the list
             processedLines.push(...results[0]);
@@ -353,7 +305,9 @@ async function addEmptyLines() {
         // Remove all trailing empty lines
         let firstTrailingEmpty: number = 0;
         for (let i: number = processedLines.length - 1; i > -1; i--) {
-            if (!isLineEmpty(processedLines[i])) {
+            const line = processedLines[i];
+
+            if (!isLineEmpty(line)) {
                 firstTrailingEmpty = i + 1;
                 break;
             }
@@ -362,7 +316,7 @@ async function addEmptyLines() {
         // Only apply changes if a trailing empty line isn't focused
         if (focusedLine < firstTrailingEmpty) {
             const lineCount: number = processedLines.length;
-            
+
             // Add empty lines if not enough, else remove excessive empty lines
             if (firstTrailingEmpty + emptyLinesBehind > lineCount) {
                 for (let i: number = firstTrailingEmpty; i < lineCount; i++) {
@@ -374,7 +328,7 @@ async function addEmptyLines() {
                 }
             }
         }
-	}
+    }
 
     // Replace the document content with the modified content
     const edit: vscode.WorkspaceEdit = new vscode.WorkspaceEdit();
@@ -382,11 +336,13 @@ async function addEmptyLines() {
         new vscode.Position(0, 0),
         new vscode.Position(totalLines, document.lineAt(totalLines - 1).text.length)
     );
-	edit.replace(document.uri, fullRange, processedLines.join(os.EOL));
-	
+    edit.replace(document.uri, fullRange, processedLines.join(os.EOL));
+
     // Apply changes
+    debounce = true;
     await vscode.workspace.applyEdit(edit);
     await document.save();
+    debounce = false;
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -397,11 +353,10 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(disposable);
 
-    // Add empty lines when a document is saved
+    // Call the main function when a document is saved
     vscode.workspace.onDidSaveTextDocument(() => {
         addEmptyLines();
     });
 }
 
-export function deactivate() {}
-
+export function deactivate() { }
